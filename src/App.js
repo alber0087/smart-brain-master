fa17e973e0ede9bb96d978feb742ff330071a603
albertorg0087 <albertorg0087@gmail.com>
2022-03-16 10:43:31 +0000

    updated db

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://afternoon-wave-88423.herokuapp.com/imageurl', {
+      fetch('https://quiet-plateau-94979.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://afternoon-wave-88423.herokuapp.com/image', {
+          fetch('https://quiet-plateau-94979.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
